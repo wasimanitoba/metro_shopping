c3a13def6661c0c88d27304268d0f6217214e66d
Wasim Adetunji <liberalpartywes@live.ca>
2023-01-04 14:01:47 +0000

    Demo version of shopping list cart working

--- app/javascript/controllers/shopping_controller.js
+++ app/javascript/controllers/shopping_controller.js
@@ -1,15 +1,14 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static outlets = [ "result" ]
+  static outlets = [ "selectable" ]
 
   generateShoppingList(evt) {
-    let items = this.resultOutlet.selectable.getSelectedItems().map((i) => i.node.id );
+    let items = this.selectableOutlet.selectable.getSelectedItems().map((i) => i.node.id );
     let form  = evt.currentTarget;
     // have to check for pre-existing value and update it
     form.action += `?${new URLSearchParams({ selected_item_ids: items })}`
-    console.log(items, form, form.action);
 
-    // form.requestSubmit();
+    form.requestSubmit();
   }
 }
