4f7a8ca2c91b8a41a3fce612a6a96c6216e52f3f
Wasim Adetunji <liberalpartywes@live.ca>
2023-01-08 19:13:49 +0000

    Added table interactive features

--- app/javascript/controllers/table_controller.js
+++ app/javascript/controllers/table_controller.js
@@ -4,6 +4,13 @@ import {
   DownloadModule,
   ExportModule,
   HtmlTableImportModule,
+  MenuModule,
+  MoveColumnsModule,
+  MoveRowsModule,
+  PrintModule,
+  PageModule,
+  ResizeTableModule,
+  ResponsiveLayoutModule,
   SortModule,
   Tabulator,
 } from "tabulator-tables";
@@ -13,6 +20,13 @@ Tabulator.registerModule([
   DownloadModule,
   ExportModule,
   HtmlTableImportModule,
+  MenuModule,
+  MoveColumnsModule,
+  MoveRowsModule,
+  PrintModule,
+  ResizeTableModule,
+  ResponsiveLayoutModule,
+  PageModule,
   SortModule
 ]);
 
@@ -21,27 +35,50 @@ export default class extends Controller {
   static targets = ['container']
 
   downloadCSV() {
-    this.table.download("csv", "data.csv");
+    self.table.download("csv", "data.csv");
   }
 
   downloadPDF() {
-    this.table.download("pdf", "data.pdf", {
+    self.table.download("pdf", "data.pdf", {
       orientation: "portrait",
-      title: this.titleValue,
+      title: self.titleValue,
     });
   }
 
-  downloadXLSX() { this.table.download("xlsx", "data.xlsx", { sheetName: this.titleValue }); }
-  downloadJSON() { this.table.download("json", "data.json"); }
-  downloadHTML() { this.table.download("html", "data.html", { style: true }); }
+  downloadXLSX() { self.table.download("xlsx", "data.xlsx", { sheetName: self.titleValue }); }
+  downloadJSON() { self.table.download("json", "data.json"); }
+  downloadHTML() { self.table.download("html", "data.html", { style: true }); }
 
   connect() {
-    let settings = { layout: "fitColumns", addRowPos: "top" }
+    const self = this;
 
-    if (this.hasHeightValue)  settings.maxHeight = this.heightValue;
-    if (this.hasUrlValue)     settings.ajaxURL   = this.urlValue;
-    if (this.hasColumnsValue) settings.columns   = this.columnsValue;
+    let headerMenu = [
+      {
+        label:"Hide Column",
+        action: function(e, column){
+            column.hide();
+        }
+      },
+    ]
 
-    this.table = new Tabulator(`#${this.containerTarget.id}`, settings);
+    let settings = {
+      layout: "fitColumns",
+      movableColumns:true,
+      movableRows: true,
+      paginationSize: 2,
+      paginationCounter:"rows",
+      responsiveLayout: true,
+      autoResize: false,
+    }
+
+    if (self.hasHeightValue)  settings.maxHeight = self.heightValue;
+    if (self.hasUrlValue)     settings.ajaxURL   = self.urlValue;
+    if (self.hasColumnsValue) settings.columns   = self.columnsValue;
+
+    self.table = new Tabulator(`#${self.containerTarget.id}`, settings);
+
+    self.table.on("dataLoaded", function(_data){
+      self.table.getColumns().forEach(column => { column.updateDefinition({ headerMenu: headerMenu }); });
+    });
   }
 }
